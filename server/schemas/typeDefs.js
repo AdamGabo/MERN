17b338c430f85789bf67abd461aa4a7bc1af6e97
Adam Gaboury <adam.gaboury@hotmail.com>
2022-11-20 13:25:30 -0500

    double check

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,13 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-
+    type Auth {
+        token: ID!
+        user: User
+    }
   type Thought {
     _id: ID
     thoughtText: String
@@ -34,10 +38,19 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
